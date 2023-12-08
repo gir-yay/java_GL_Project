ff013c09d20ff87f019fb7c6117847d33d4e1a71
Mohammed EZ-ZOUAK <ezzouak2001@gmail.com>
2023-12-09 00:36:54 +0100

    u can now see the file from the history

--- p1/src/p1/Historique.java
+++ p1/src/p1/Historique.java
@@ -9,9 +9,13 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.swing.JOptionPane;
+import javax.swing.RowFilter;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 
 /**
  *
@@ -32,7 +36,15 @@ public class Historique extends javax.swing.JFrame {
     public void setDashboardInstance(Dashbrd DashboardInstance) {
         this.DashboardInstance = DashboardInstance;
     }
+    
+    public void actualiser() {
+        // clear it first then fill()
+        DefaultTableModel model = (DefaultTableModel) jTable2.getModel();
+        model.setRowCount(0); // Clear the table before adding rows
+        fill();
+    }
 
+    // used to fill the table
     public void fill() {
         try {
             Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/gl", "root", "");
@@ -41,17 +53,14 @@ public class Historique extends javax.swing.JFrame {
                     "SELECT demande_ar.id,student.Nom_complet,student.email,student.CNE FROM demande_ar INNER JOIN student ON demande_ar.user_id = student.CNE where traité = '1';");
             DefaultTableModel model = (DefaultTableModel) jTable2.getModel();
             model.setRowCount(0); // Clear the table before adding rows
-
             while (rs.next()) {
-                System.out.println("Row from database: " + rs.getInt("id") + ", " + rs.getString("Nom_complet") + ", "
-                        + rs.getString("email") + ", Attestation de réussite"); // Debug print
-                model.addRow(new Object[] { rs.getString("Nom_complet"), rs.getInt("student.CNE"),
-                        rs.getString("email"), "Date", "Attestation de réussite" });
+                model.addRow(new Object[] { rs.getString("id"), rs.getString("Nom_complet"),
+                        rs.getInt("student.CNE"),
+                        rs.getString("email"), "Attestation de réussite" });
             }
             jTable2.revalidate();
             jTable2.repaint();
-
-            System.out.println("Table should be updated now.");
+            System.out.println("Added Attestation de réussite to the table.");
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -64,28 +73,64 @@ public class Historique extends javax.swing.JFrame {
             DefaultTableModel model = (DefaultTableModel) jTable2.getModel();
 
             while (rs.next()) {
-                System.out.println("Row from database: " + rs.getInt("id") + ", " + rs.getString("Nom_complet") + ", "
-                        + rs.getString("email") + ", Relevé de notes"); // Debug print
-                model.addRow(new Object[] { rs.getString("Nom_complet"), rs.getInt("student.CNE"),
-                        rs.getString("email"), "Date", "Relevé de notes" });
+                model.addRow(new Object[] { rs.getString("id"), rs.getString("Nom_complet"),
+                        rs.getInt("student.CNE"),
+                        rs.getString("email"), "Relevé de notes" });
+            }
+            jTable2.revalidate();
+            jTable2.repaint();
+            System.out.println("Added Relevé de notes to the table.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        // Attestation de stage table
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/gl", "root", "");
+            java.sql.Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(
+                    "SELECT stage.id,student.Nom_complet,student.email,student.CNE FROM stage INNER JOIN student ON stage.user_id = student.CNE where traité = '1';");
+            DefaultTableModel model = (DefaultTableModel) jTable2.getModel();
+
+            while (rs.next()) {
+                model.addRow(new Object[] { rs.getString("id"), rs.getString("Nom_complet"),
+                        rs.getInt("student.CNE"),
+                        rs.getString("email"), "Attestation de stage", });
             }
             jTable2.revalidate();
             jTable2.repaint();
 
-            System.out.println("Table should be updated now.");
+            System.out.println("Added Attestation de stage to the table.");
         } catch (SQLException e) {
             e.printStackTrace();
-        } catch (Exception e) {
-            // TODO: handle exception
+        }
+        // demande_as table
+        try {
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/gl", "root", "");
+            java.sql.Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(
+                    "SELECT demande_as.id,student.Nom_complet,student.email,student.CNE FROM demande_as INNER JOIN student ON demande_as.user_id = student.CNE where traite = '1';");
+            DefaultTableModel model = (DefaultTableModel) jTable2.getModel();
+
+            while (rs.next()) {
+                model.addRow(new Object[] { rs.getString("id"), rs.getString("Nom_complet"),
+                        rs.getInt("student.CNE"),
+                        rs.getString("email"), "Attestation de scolarité" });
+            }
+            jTable2.revalidate();
+            jTable2.repaint();
+
+            System.out.println("Added Attestation de scolarité to the table.");
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
     }
-
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
      * regenerated by the Form Editor.
      */
     @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated
     // <editor-fold defaultstate="collapsed" desc="Generated
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -122,21 +167,23 @@ public class Historique extends javax.swing.JFrame {
 
         jTable2.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null},
-                {null, null, null, null, null}
             },
             new String [] {
-                "ID", "Nom Prenom", "Email", "Type de document", "Date"
+                "ID", "Nom Prenom", "CNE","Email", "Type de document",
             }
         ));
+        fill();
         jScrollPane2.setViewportView(jTable2);
 
         jButton3.setBackground(new java.awt.Color(51, 51, 255));
         jButton3.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         jButton3.setForeground(new java.awt.Color(255, 255, 255));
         jButton3.setText("Telecharger");
+        jButton3.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton3MouseClicked(evt);
+            }
+        });
         jButton3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton3ActionPerformed(evt);
@@ -282,12 +329,65 @@ public class Historique extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void jButton3MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton3MouseClicked
+        //get the type of doc from the table
+        String type = jTable2.getValueAt(jTable2.getSelectedRow(), 4).toString();
+        //get the cne from the table
+        Integer cne = Integer.parseInt(jTable2.getValueAt(jTable2.getSelectedRow(), 2).toString());
+        //depend on the type of doc
+        switch (type) {
+            case "Attestation de réussite":
+                //open the filein the output folder Attestation_de_réussite + cne.toString() + .pdf
+                try {
+                    Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + "C:\\Users\\ezzou\\OneDrive\\Desktop\\output\\Attestation_de_réussite "+ cne.toString() + ".pdf");
+                } catch (Exception e) {
+                    System.out.println("Error opening the file");
+                }
+                break;
+            case "Relevé de notes":
+                //open the filein the output folder Relevé_de_notes + cne.toString() + .pdf
+                try {
+                    Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + "C:\\Users\\ezzou\\OneDrive\\Desktop\\output\\Relevé_de_notes "+ cne.toString() + ".pdf");
+                } catch (Exception e) {
+                    System.out.println("Error opening the file");
+                }
+                break;
+            case "Attestaion de scolarité":
+                //open the filein the output folder Attestaion_de_scolarité + cne.toString() + .pdf
+                try {
+                    Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + "C:\\Users\\ezzou\\OneDrive\\Desktop\\output\\Attestaion_de_scolarité "+ cne.toString() + ".pdf");
+                } catch (Exception e) {
+                    System.out.println("Error opening the file");
+                }
+                break;
+            case "Attestation de stage":
+                //open the filein the output folder Attestation_de_stage + cne.toString() + .pdf
+                try {
+                    Runtime.getRuntime().exec("rundll32 url.dll,FileProtocolHandler " + "C:\\Users\\ezzou\\OneDrive\\Desktop\\output\\Attestation_de_stage "+ cne.toString() + ".pdf");
+                } catch (Exception e) {
+                    System.out.println("Error opening the file");
+                }
+                break;
+        
+            default:
+                JOptionPane.showMessageDialog(null, "Error opening the file");
+        }
+
+    }//GEN-LAST:event_jButton3MouseClicked
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton2ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }// GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        String recherche = jTextPane1.getText();
+                TableRowSorter<TableModel> rowSorter = new TableRowSorter<>(jTable2.getModel());
+                jTable2.setRowSorter(rowSorter);
+                rowSorter.setRowFilter(RowFilter.regexFilter(recherche));
+        //if the field is empty call actualiser()
+        if (recherche.equals("")) {
+            actualiser();
+        }
     }// GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {// GEN-FIRST:event_jButton3ActionPerformed
